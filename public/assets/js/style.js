65bc778a1d9a5c247cab170036a141905beb030c
juan peron <somostelam23@gmail.com>
2018-07-02 00:16:57 -0300

    estilos

--- public/assets/js/style.js
+++ public/assets/js/style.js
@@ -1,9 +1,13 @@
 function main(){
+  $(".desktop-menu").css({"height": $(window).height()+"px"});
+  $(window).on("resize",function(){
+    $(".desktop-menu").css({"height": $(window).height()+"px"});
+  });
   $(".foto-lista").on("click", function(){
-    $(this).siblings(".perfil-modal").toggle("blind");
+    $(this).siblings(".perfil-modal").toggle("bounce");
   })
   $(".perfil-modal").on("click",function(){
-    $(this).toggle("blind");
+    $(this).toggle("bounce");
   })
   $(".perfil-modal").css({"height": $(window).height()+"px", "width": $(window).width()+"px"});
 }
